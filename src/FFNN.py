fix backpropagation, remove redundancies

src/FFNN.py
@@ -12,33 +12,34 @@ from sklearn.utils import resample
 
 class FFNN:
     """
-    Description: 
+    Description:
     ------------
-        Feed Forward Neural Network with interface enabling flexible design of a 
-        nerual networks architecture and the specification of activation function 
-        in the hidden layers and output layer respectively. This model can be used 
+        Feed Forward Neural Network with interface enabling flexible design of a
+        nerual networks architecture and the specification of activation function
+        in the hidden layers and output layer respectively. This model can be used
         for both regression and classification problems, depending on the output function.
 
     Attributes:
     ------------
-        I.  dimensions (tuple[int]): A list of positive integers, which specifies the 
-            number of nodes in each of the networks layers. The first integer in the array 
-            defines the number of nodes in the input layer, the second integer defines number 
-            of nodes in the first hidden layer and so on until the last number, which 
+        I.  dimensions (tuple[int]): A list of positive integers, which specifies the
+            number of nodes in each of the networks layers. The first integer in the array
+            defines the number of nodes in the input layer, the second integer defines number
+            of nodes in the first hidden layer and so on until the last number, which
             specifies the number of nodes in the output layer.
         II. hidden_func (Callable): The activation function for the hidden layers
         III.output_func (Callable): The activation function for the output layer
         IV. cost_func (Callable): Our cost function
-        V.  weights (list): A list of numpy arrays, containing our weights
+        V.  seed (int): Sets random seed, makes results reproducible
     """
+
     def __init__(
         self,
         dimensions: tuple[int],
         hidden_func: Callable = sigmoid,
         output_func: Callable = lambda x: x,
         cost_func: Callable = CostOLS,
-        seed: int = None, # Used for reproducibility 
-     ):
+        seed: int = None, 
+    ):
         self.dimensions = dimensions
         self.hidden_func = hidden_func
         self.output_func = output_func
@@ -50,25 +51,13 @@ class FFNN:
         self.a_matrices = list()
         self.z_matrices = list()
 
-        # The initialization of weights for the Neural Network is done 
-        # the moment an object of FFNN class is created
-        for i in range(len(self.dimensions) - 1):
-            if self.seed is not None:
-                np.random.seed(self.seed)
-            weight_array = np.random.randn(
-                self.dimensions[i] + 1, self.dimensions[i + 1]
-            )
-            # Matrix containing weights for the i-th layer in the network
-            weight_array[0, :] = np.random.randn(self.dimensions[i + 1]) * 0.1
-            
-            self.weights.append(weight_array)
-
+        self._initialize_weights()
 
     def fit(
         self,
-        X: np.ndarray, 
+        X: np.ndarray,
         t: np.ndarray,
-        scheduler_class: Scheduler, 
+        scheduler_class: Scheduler,
         *scheduler_args: list(),
         batches: int = 1,
         epochs: int = 100,
@@ -77,12 +66,12 @@ class FFNN:
         t_val: np.ndarray = None,
     ):
         """
-        Description: 
-        ------------ 
+        Description:
+        ------------
             This function performs the training the neural network by performing the feedforward and backpropagation
-            algorithm to update the networks weights. 
+            algorithm to update the networks weights.
 
-        Parameters: 
+        Parameters:
         ------------
             I    X (np.ndarray) : training data
             II   t (np.ndarray) : target data
@@ -97,9 +86,9 @@ class FFNN:
             VIII X_val (np.ndarray) : validation set
             IX   t_val (np.ndarray) : validation target set
 
-        Returns: 
+        Returns:
         ------------
-            I.  scores (dict) : A dictionary containing the performance metrics of the model. The number of the metrics 
+            I.  scores (dict) : A dictionary containing the performance metrics of the model. The number of the metrics
                 depends on the parameters passed to the fit-function.
 
         """
@@ -113,7 +102,7 @@ class FFNN:
             self.cost_func.__name__ == "CostLogReg"
             or self.cost_func.__name__ == "CostCrossEntropy"
         ):
-            classification = True 
+            classification = True
 
         val_set = False
         if X_val is not None and t_val is not None:
@@ -153,7 +142,7 @@ class FFNN:
             self.schedulers_bias.append(scheduler_class(*scheduler_args))
 
         print(f"{scheduler_class.__name__}: Eta={scheduler_args[0]}, Lambda={lam}")
-        # this try allows to abort the code early by pressing Ctrl+c
+
         try:
             for e in range(epochs):
                 for i in range(batches):
@@ -165,7 +154,7 @@ class FFNN:
                     else:
                         X_batch = X[i * batch_size : (i + 1) * batch_size, :]
                         t_batch = t[i * batch_size : (i + 1) * batch_size, :]
-                    
+
                     self._feedforward(X_batch)
                     self._backpropagate(X_batch, t_batch, lam)
 
@@ -176,8 +165,10 @@ class FFNN:
                 for scheduler in self.schedulers_bias:
                     scheduler.reset()
 
+                # TODO should be generalizeable to regression (top part) and classification
+                # or no generalization needed and its just confusing
                 # --------- Computing performance metrics ---------
-                prediction = self.predict(X, regression=True)
+                prediction = self.predict(X)
                 train_error = cost_function_train(prediction)
                 if train_error > 10e20:
                     # Indicates a problem with the training
@@ -188,7 +179,7 @@ class FFNN:
                     val_acc = None
                     raise OverflowError
                 if val_set:
-                    prediction_test = self.predict(X_val, regression=True)
+                    prediction_test = self.predict(X_val)
                     val_error = cost_function_test(prediction_test)
                     best_val_error = val_error
                     best_train_error = train_error
@@ -198,32 +189,32 @@ class FFNN:
 
                 train_acc = None
                 val_acc = None
+
                 if classification:
-                    train_acc = self._accuracy(self.predict(X, regression=False), t)
+                    train_acc = self._accuracy(self.predict(X), t)
                     train_accs[e] = train_acc
                     if val_set:
-                        val_acc = self._accuracy(self.predict(X_val, regression=False), t_val)
+                        val_acc = self._accuracy(
+                            self.predict(X_val), t_val
+                        )
                         val_accs[e] = val_acc
                         best_test_acc = val_acc
                         best_train_acc = train_acc
 
                 train_errors[e] = train_error
+
+                # ----- printing progress bar ------------
                 if not val_set:
                     progression = e / epochs
-
-                    # ----- printing progress bar ------------
                     length = self._progress_bar(
                         progression,
                         train_error=train_error,
                         train_acc=train_acc,
                         val_acc=val_acc,
                     )
-
-                if val_set:
+                else:
                     val_errors[e] = val_error
                     progression = e / epochs
-
-                    # ----- printing progress bar ------------
                     length = self._progress_bar(
                         progression,
                         train_error=train_error,
@@ -274,37 +265,32 @@ class FFNN:
 
         return scores
 
-
-    def predict(self, X: np.ndarray, *, regression=False, threshold=0.5):
+    def predict(self, X: np.ndarray, *, threshold=0.5):
         """
-        Description: 
-        ------------
-            Performs prediction after training of the network has been finished.
+         Description:
+         ------------
+             Performs prediction after training of the network has been finished.
 
-        Parameters:
-       ------------
-            I.  X (np.ndarray): The design matrix, with n rows of p features each
-
-        Optional Parameters:
+         Parameters:
         ------------
-            II. regression (boolean) : if set to True, performs prediction for regression problems
-            III.threshold (float) : sets minimal value for a prediction to be predicted as the positive class
-                in classification problems
+             I.  X (np.ndarray): The design matrix, with n rows of p features each
 
-        Returns:
-        ------------
-            I.  z (np.ndarray): A prediction vector (row) for each row in our design matrix
-                This vector is thresholded if regression=False, meaning that classification results
-                in a vector of 1s and 0s, while regressions in an array of decimal numbers
+         Optional Parameters:
+         ------------
+             II. threshold (float) : sets minimal value for a prediction to be predicted as the positive class
+                 in classification problems
+
+         Returns:
+         ------------
+             I.  z (np.ndarray): A prediction vector (row) for each row in our design matrix
+                 This vector is thresholded if regression=False, meaning that classification results
+                 in a vector of 1s and 0s, while regressions in an array of decimal numbers
 
         """
 
         predict = self._feedforward(X)
-        # boolean == True is equivalent with the model performing regression 
-        # (in other words, having no activation function in the output layer)
-        if regression:
-            return predict
-        elif (
+
+        if (
             self.cost_func.__name__ == "CostLogReg"
             or self.cost_func.__name__ == "CostCrossEntropy"
         ):
@@ -312,13 +298,12 @@ class FFNN:
         else:
             return predict
 
-
     def reset_weights(self):
         """
-        Description: 
+        Description:
         ------------
-            Resets/Reinitializes the weights in order to train the network for a new problem. 
-        
+        Resets/Reinitializes the weights in order to train the network for a new problem.
+
         """
         if self.seed is not None:
             np.random.seed(self.seed)
@@ -332,21 +317,20 @@ class FFNN:
 
             self.weights.append(weight_array)
 
-
     def _feedforward(self, X: np.ndarray):
         """
-        Description: 
+        Description:
         ------------
-            Calculates the activation of each layer starting at the input and ending at the output. 
-            Each following activation is calculated from a weighted sum of each of the preceeding 
-            activations (except in the case of the input layer). 
-        
+            Calculates the activation of each layer starting at the input and ending at the output.
+            Each following activation is calculated from a weighted sum of each of the preceeding
+            activations (except in the case of the input layer).
+
         Parameters:
         ------------
             I.  X (np.ndarray): The design matrix, with n rows of p features each
-        
+
         Returns:
-        ------------    
+        ------------
             I.  z (np.ndarray): A prediction vector (row) for each row in our design matrix
         """
 
@@ -354,16 +338,16 @@ class FFNN:
         self.a_matrices = list()
         self.z_matrices = list()
 
-        # if X is just a vector, make it into a design matrix
+        # if X is just a vector, make it into a matrix
         if len(X.shape) == 1:
             X = X.reshape((1, X.shape[0]))
 
-        # Add a coloumn of zeros as the first coloumn of the design matrix, in order 
-        # to add bias to our data 
+        # Add a coloumn of zeros as the first coloumn of the design matrix, in order
+        # to add bias to our data
         bias = np.ones((X.shape[0], 1)) * 0.01
         X = np.hstack([bias, X])
 
-        # a^0, the nodes in the input layer (one a^0 for each row in X - where the 
+        # a^0, the nodes in the input layer (one a^0 for each row in X - where the
         # exponent indicates layer number).
         a = X
         self.a_matrices.append(a)
@@ -377,7 +361,7 @@ class FFNN:
                 a = self.hidden_func(z)
                 # bias column again added to the data here
                 bias = np.ones((a.shape[0], 1)) * 0.01
-                a = np.hstack([bias, a]) 
+                a = np.hstack([bias, a])
                 self.a_matrices.append(a)
             else:
                 # a^L, the nodes in our output layer
@@ -389,17 +373,16 @@ class FFNN:
         # this will be a^L
         return a
 
-
     def _backpropagate(self, X, t, lam):
         """
-        Description: 
+        Description:
         ------------
             Performs the backpropagation algorithm. In other words, this method
-            calculates the gradient of all the layers starting at the 
+            calculates the gradient of all the layers starting at the
             output layer, and moving from right to left accumulates the gradient until
-            the input layer is reached. Each layers respective weights are updated while 
+            the input layer is reached. Each layers respective weights are updated while
             the algorithm propagates backwards from the output layer (auto-differentation in reverse mode).
-        
+
         Parameters:
         ------------
             I   X (np.ndarray): The design matrix, with n rows of p features each.
@@ -408,22 +391,22 @@ class FFNN:
 
         Returns:
         ------------
-            No return value. 
+            No return value.
 
         """
         out_derivative = derivate(self.output_func)
         hidden_derivative = derivate(self.hidden_func)
-        update_list = list()
 
-        # creating the delta terms
         for i in range(len(self.weights) - 1, -1, -1):
             # delta terms for output
             if i == len(self.weights) - 1:
+                # for multi-class classification
                 if (
                     self.output_func.__name__ == "softmax"
                     and self.cost_func.__name__ == "CostCrossEntropy"
                 ):
                     delta_matrix = self.a_matrices[i + 1] - t
+                # for single class classification
                 else:
                     cost_func_derivative = grad(self.cost_func(t))
                     delta_matrix = out_derivative(
@@ -436,39 +419,25 @@ class FFNN:
                     self.weights[i + 1][1:, :] @ delta_matrix.T
                 ).T * hidden_derivative(self.z_matrices[i + 1])
 
-            gradient_weights_matrix = np.zeros(
-                (
-                    self.a_matrices[i][:, 1:].shape[0],
-                    self.a_matrices[i][:, 1:].shape[1],
-                    delta_matrix.shape[1],
-                )
+            # calculate gradient
+            gradient_weights = self.a_matrices[i][:, 1:].T @ delta_matrix
+            gradient_bias = np.sum(delta_matrix, axis=0).reshape(
+                1, delta_matrix.shape[1]
             )
 
-            for j in range(len(delta_matrix)):
-                gradient_weights_matrix[j, :, :] = np.outer(
-                    self.a_matrices[i][j, 1:], delta_matrix[j, :]
-                )
-
-            gradient_weights = np.mean(gradient_weights_matrix, axis=0)
-            delta_accumulated = np.mean(delta_matrix, axis=0)
-
+            # regularization term
             gradient_weights += self.weights[i][1:, :] * lam
 
+            # use scheduler
             update_matrix = np.vstack(
                 [
-                    self.schedulers_bias[i].update_change(
-                        delta_accumulated.reshape(1, delta_accumulated.shape[0])
-                    ),
+                    self.schedulers_bias[i].update_change(gradient_bias),
                     self.schedulers_weight[i].update_change(gradient_weights),
                 ]
             )
 
-            update_list.insert(0, update_matrix)
-
-        # update weights and biases
-        for i in range(len(self.weights)):
-            self.weights[i] -= update_list[i]
-
+            # update weights and bias
+            self.weights[i] -= update_matrix
 
     def _accuracy(self, prediction: np.ndarray, target: np.ndarray):
         """
@@ -477,6 +446,20 @@ class FFNN:
         assert prediction.size == target.size
         return np.average((target == prediction))
 
+    def _initialize_weights(self):
+        """
+        Initializes weights of the FFNN, called upon object initiation
+        """
+        for i in range(len(self.dimensions) - 1):
+            if self.seed is not None:
+                np.random.seed(self.seed)
+            weight_array = np.random.randn(
+                self.dimensions[i] + 1, self.dimensions[i + 1]
+            )
+            # Matrix containing weights for the i-th layer in the network
+            weight_array[0, :] = np.random.randn(self.dimensions[i + 1]) * 0.1
+
+            self.weights.append(weight_array)
 
     def _progress_bar(self, progression, **kwargs):
         """
@@ -496,7 +479,6 @@ class FFNN:
                 line += f"| {key}: {value} "
         print(line, end="\r")
         return len(line)
-
 
     def _fmt(self, value, N=4):
         """